0053d6fd20109d7a08bfb705bf6efef579a00a04
Pithakote <muttsang@gmail.com>
2019-01-14 15:04:36 +0000

    added health and enemy spawns

--- Kill Em All/Assets/scripts/enemyBullet.cs
+++ Kill Em All/Assets/scripts/enemyBullet.cs
@@ -5,30 +5,44 @@ using UnityEngine;
 public class enemyBullet : MonoBehaviour {
 
     public float speed;
-
+    public float lifeTime;
     private Transform player;
     private Vector2 target;
-	// Use this for initialization
-	void Start () {
+    public float distance;
+    public LayerMask solid;
+    // Use this for initialization
+    void Start () {
+        Invoke("destroyProjectile", lifeTime);
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
             target = new Vector2(player.position.x, player.position.y);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, solid);
 
         if (transform.position.x == target.x && transform.position.y == target.y)
         {
             DestroyProjectile();
         }
-	}
+
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                hitInfo.collider.GetComponent<playerMovement>().takeDmg(1);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag ("Player"))
         {
+           
             DestroyProjectile();
         }
     }
--- Kill Em All/Assets/scripts/playerMovement.cs
+++ Kill Em All/Assets/scripts/playerMovement.cs
@@ -1,22 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class playerMovement : MonoBehaviour {
 
 
     public float speed;
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
+    public GameObject deathParticle;
+    public int health ;
+    public TMP_Text healthtext;
+    //public GameObject healthText;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
 	}
-	
+    public void takeDmg(int damage)
+    {
+        health -= damage;
+    }
 	// Update is called once per frame
 	void Update () {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveVelocity = move.normalized * speed;
+
+        healthtext.text = "Health:" + health;
+        if (health <= 0)
+        {
+            Instantiate(deathParticle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        else
+        {
+
+            Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            moveVelocity = move.normalized * speed;
+        }
     }
 
     private void FixedUpdate()
